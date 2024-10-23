e1da84df039587aef20cee63937f309def40b2a4
josuengwala <josue.ngwala@findit.fr>
2024-10-23 09:56:42 +0200

    [RED] Les personnages ont 10HP au départ

--- test/test_rpg.py
+++ test/test_rpg.py
@@ -1,9 +1,10 @@
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestRpg(unittest.TestCase):
+    def test_10_hp_initiaux(self):
+        personnage = Personnage()
+        self.assertEqual(10, personnage.get_hp())
 
 
 if __name__ == '__main__':
