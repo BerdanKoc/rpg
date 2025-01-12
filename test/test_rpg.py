03d8699cbd92207c72718a09f82ffa81d8b7ca5e
azizchouikha <ahmedaziz.chouikha@ecoles-epsi.net>
2025-01-12 15:01:25 +0100

    test: add a test to initiate a fight between two characters

--- test/test_rpg.py
+++ test/test_rpg.py
@@ -121,6 +121,13 @@ class TestRpg(unittest.TestCase):
         defenseur.recevoir_attaque(attaquant)
         self.assertEqual(7, defenseur.get_hp())  # 10 HP - 3 dégâts
 
+    def test_initier_combat(self):
+        attaquant = Personnage()
+        defenseur = Personnage()
+        
+        attaquant.combattre(defenseur)
+        self.assertTrue(True)  
+
 
 if __name__ == '__main__':
     unittest.main()
